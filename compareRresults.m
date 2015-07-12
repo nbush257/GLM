52d9518908b4946395ab0b002e8172d8073e2f1d
nbush257 <nbush257@gmail.com>
2015-07-12 18:09:44 -0500

    Added new analyses to comparisons

--- compareRresults.m
+++ compareRresults.m
@@ -1,3 +1,12 @@
+% Names
+names = {}
+for ii = 1:length(d)
+    if ~isempty(regexp(d(ii).name,'Rat1*'))
+        names{ii} = d(ii).name(9:16)
+    else
+        names{ii} = d(ii).name([9:10 21 22 28:33])
+    end
+end
 % compare ROC
 for ii = 1:37
     subplot(7,6,ii)
@@ -5,42 +14,102 @@ for ii = 1:37
     ho
     plot(mech(ii).ROC_x,mech(ii).ROC_y)
     ho
+    title(names{ii},'Interpreter','None')
     dline
 end
 % compare AUC
-figure
+subplot(221)
 plot([[mech.AUC];[geo.AUC]],'k')
-figure
+set(gca,'XTickLabel',{'Mech','Geo'})
+title('All')
+ylabel('AUC')
+subplot(222)
 plot([[m_No_Deriv.AUC];[g_No_Deriv.AUC]],'k')
+title('No Derivative')
+set(gca,'XTickLabel',{'Mech','Geo'})
+ylabel('AUC')
+subplot(223)
+plot([[m_JustDeriv.AUC];[g_JustDeriv.AUC]],'k')
+title('Just Derivative')
+ylabel('AUC')
+set(gca,'XTickLabel',{'Mech','Geo'})
 
 
-
+%%
 figure
-mG = mean([geo.AUC]);
-mGJ = mean([g_JustDeriv.AUC]);
-mGN = mean([g_No_Deriv.AUC]);
+mG = mean([geo.AUC]);sG = std([geo.AUC])/sqrt(numel([geo.AUC]));
+mGJ = mean([g_JustDeriv.AUC]);sGJ = std([g_JustDeriv.AUC])/sqrt(numel([g_JustDeriv.AUC]));
+mGN = mean([g_No_Deriv.AUC]);sGN = std([g_No_Deriv.AUC])/sqrt(numel([g_No_Deriv.AUC]));
 
-mM = mean([mech.AUC]);
-mMJ = mean([m_JustDeriv.AUC]);
-mMN = mean([m_No_Deriv.AUC]);
-bar([mG mM;mGJ mMJ; mGN mMN])
+mM = mean([mech.AUC]);sM = std([mech.AUC])/sqrt(numel([mech.AUC]));
+mMJ = mean([m_JustDeriv.AUC]);sMJ = std([m_JustDeriv.AUC])/sqrt(numel([m_JustDeriv.AUC]));
+mMN = mean([m_No_Deriv.AUC]);sMN = std([m_No_Deriv.AUC])/sqrt(numel([m_No_Deriv.AUC]));
+%bar([mG mM;mGJ mMJ; mGN mMN]);
+ho
+errorbar_groups([mG mGJ mGN;mM mMJ mMN],[sG sGJ sGN ;sM sMJ sMN])
+
 set(gca,'XTickLabel',{'All','Just Deriv','No Deriv'})
 legend({'Geo','Mech'})
 title('Area Under ROC')
+hline(.5)
+set(gca,'FontSize',20)
 
 %% rates
 rate = struct;
 rate.mech = [];
 rate.geo = [];
 rate.spike = [];
-
+bin = 5;
 
 for ii = 1:37
-    rate(ii).mech = tsmovavg(mech(ii).P','s',15);
-    rate(ii).geo= tsmovavg(geo(ii).P','s',15);
-    rate(ii).spike = tsmovavg(mech(ii).tested_spikes','s',15);
+    rate(ii).mech = tsmovavg(mech(ii).P','s',bin);
+    rate(ii).geo= tsmovavg(geo(ii).P','s',bin);
+    rate(ii).spike = tsmovavg(mech(ii).tested_spikes','s',bin);
     rate(ii).geo(isnan(rate(ii).geo)) = 0;
     rate(ii).spike(isnan(rate(ii).spike)) = 0;
     rate(ii).mech(isnan(rate(ii).mech)) = 0;
     
 end
+
+%% Predictive mode
+figure
+for ii = 1:length(geo)
+    ii
+    [G(ii).x,G(ii).y,~,G(ii).AUC] = perfcurve(geo(ii).tested_spikes,geo(ii).predictiveModeY,'1');
+    [M(ii).x,M(ii).y,~,M(ii).AUC] = perfcurve(mech(ii).tested_spikes,mech(ii).predictiveModeY,'1');
+    subplot(6,7,ii)
+    plot(G(ii).x,G(ii).y)
+    ho
+    plot(M(ii).x,M(ii).y)
+    title(names{ii})
+end
+
+%% No Hist
+nl = @(x) 1./(1 +exp(-x));
+for ii = 1:length(geo)
+geoLogit{ii} = nl(geo(ii).Y);
+mechLogit{ii} = nl(mech(ii).Y);
+end
+
+for ii = 1:length(geo)
+    ii
+    [G_noHist(ii).x,G_noHist(ii).y,~,G_noHist(ii).AUC] = perfcurve(geo(ii).tested_spikes,geoLogit{ii},'1');
+    [M_noHist(ii).x,M_noHist(ii).y,~,M_noHist(ii).AUC] = perfcurve(mech(ii).tested_spikes,mechLogit{ii},'1');
+end
+
+figure
+for ii = 1:length(geo)
+    subplot(6,7,ii)
+    plot(G_noHist(ii).x,G_noHist(ii).y)
+    ho
+    plot(M_noHist(ii).x,M_noHist(ii).y)
+    title(names{ii})
+end
+
+mG_noHist = mean([G_noHist.AUC]);  sG_noHist = std([G_noHist.AUC])/sqrt(numel([G_noHist.AUC]));
+mM_noHist = mean([M_noHist.AUC]);  sM_noHist = std([M_noHist.AUC])/sqrt(numel([M_noHist.AUC]));
+errorbar([mG_noHist mM_noHist],[sG_noHist sM_noHist]);
+plot([[G_noHist.AUC];[M_noHist.AUC]],'k')
+
+%% med v dis
+
--- getFR_by_vars.m
+++ getFR_by_vars.m
@@ -11,7 +11,7 @@ for di = 1:length(dter)
     starts = find(diff(C)==1)+1;
     stops = find(diff(C)==-1);
     
-    avgR = zeros(size(starts));rangeFX = zeros(size(starts)); rangeTH = zeros(size(starts)); rangeFY = zeros(size(starts));rangeM = zeros(size(starts));avgFR = zeros(size(starts));
+    avgR = NaN(size(starts));rangeFX = NaN(size(starts)); rangeTH = NaN(size(starts)); rangeFY = NaN(size(starts));rangeM = NaN(size(starts));avgFR = zeros(size(starts));
     if size(geo_85,2)>size(geo_85,1)
         geo_85 = geo_85';
         mech_85 = mech_85';
@@ -19,20 +19,54 @@ for di = 1:length(dter)
     
     for jj = 1:length(starts)
         avgR(jj) = nanmean(geo_85(starts(jj):stops(jj),1));
-        rangeTH(jj) = nanmax(geo_85(starts(jj):stops(jj),2));
-        rangeFX(jj) = nanmax(mech_85(starts(jj):stops(jj),1));
-        rangeFY(jj) = nanmax(mech_85(starts(jj):stops(jj),2));
-        rangeM(jj) = nanmax(mech_85(starts(jj):stops(jj),3));
+        [~,idxTH] = nanmax(abs(geo_85(starts(jj):stops(jj),2)));
+        rangeTH(jj) = geo_85(starts(jj)+idxTH-1,2);
+        
+        [~,idxFX] = nanmax(abs(mech_85(starts(jj):stops(jj),1)));
+        rangeFX(jj) = mech_85(starts(jj)+idxFX-1,1);
+        
+        [~,idxFY] = nanmax(abs(mech_85(starts(jj):stops(jj),2)));
+        rangeFY(jj) = mech_85(starts(jj)+idxFY-1,2);
+        
+        [~,idxM] = nanmax(abs(mech_85(starts(jj):stops(jj),3)));
+        rangeM(jj) = mech_85(starts(jj)+idxM -1,3);
         avgFR(jj) = nanmean(spikevec(starts(jj):stops(jj)));
     end
-    R{di} = avgR;
-    TH_o{di} = rangeTH;
-    FX{di} = rangeFX;
-    FY{di} = rangeFY;
-    M{di} = rangeM;
-    FR{di} = avgFR;
+    if length(starts)>5
+        rMdl = fitlm(avgR,avgFR);
+        thMdl = fitlm(rangeTH,avgFR);
+        fxMdl = fitlm(rangeFX,avgFR);
+        fyMdl = fitlm(rangeFY,avgFR);
+        mMdl = fitlm(rangeM,avgFR);
+        
+        Rsquared(di).R = rMdl.Rsquared.Adjusted;
+        Rsquared(di).TH = thMdl.Rsquared.Adjusted;
+        Rsquared(di).FX = fxMdl.Rsquared.Adjusted;
+        Rsquared(di).FY = fyMdl.Rsquared.Adjusted;
+        Rsquared(di).M = mMdl.Rsquared.Adjusted;
+        
+        p(di).R = rMdl.coefTest;
+        p(di).TH = thMdl.coefTest;
+        p(di).FX = fxMdl.coefTest;
+        p(di).FY = fyMdl.coefTest;
+        p(di).M = mMdl.coefTest;
+    end
+    
+        R{di} = avgR;
+        TH_o{di} = rangeTH;
+        FX{di} = rangeFX;
+        FY{di} = rangeFY;
+        M{di} = rangeM;
+        FR{di} = avgFR;
 end
-for ii = 1:40
-    plot(zscore(R{ii}),zscore(FR{ii}),'.')
-    ho
-end
+%% show that R is not valid in awake, but yes in anaesth
+awakeP = [p(1:10).R]<(.005) % 1 of 10 bonferroni p<.05
+anaesthP = [p(11:end).R]<(.05/30) % 11 of 30 bonferroni p<.05
+
+awakeR = [Rsquared(1:10).R];
+anaesthR = [Rsquared(11:end).R];
+
+nanmean(awakeR(awakeP)) % one example, .484
+nanmean(anaesthR(anaesthP))% 11 examples min= .05, max = .44 mean = .2 
+% R alone is not an accurate predictor of Firing Rate in active or passive
+% case
