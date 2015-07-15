cf186e1c4617c87d241db28efdfb059f2b4c13a0
nbush257 <nbush257@gmail.com>
2015-07-15 17:46:52 -0500

    unknown changes

--- singleVarCorr.m
+++ singleVarCorr.m
@@ -6,6 +6,7 @@ D = dir('*GLM.mat')
 binsize = 50;
 for II = 1:length(D)
     II
+    
     load(D(II).name)
     opts = statset('UseParallel',1);
     if iscolumn(spikevec); spikevec = spikevec';end
@@ -21,40 +22,38 @@ for II = 1:length(D)
     mech_85(~C,:)=0;
     geo_85(~C,:)=0;
     
-    rate_mech = tsmovavg(mech_85','s',binsize);rate_mech(:,isnan(rate_mech(1,:))) = [];
-    rate_geo = tsmovavg(geo_85','s',binsize);rate_geo(:,isnan(rate_geo(1,:))) = [];
-    rate_spike = tsmovavg(spikevec,'s',binsize);rate_spike(isnan(rate_spike)) = [];
-    
+    %     rate_mech = tsmovavg(mech_85','s',binsize);rate_mech(:,isnan(rate_mech(1,:))) = [];
+    %     rate_geo = tsmovavg(geo_85','s',binsize);rate_geo(:,isnan(rate_geo(1,:))) = [];
+    rate_spike = smoothts(spikevec,'g',length(spikevec),5);%tsmovavg(spikevec,'s',binsize);rate_spike(isnan(rate_spike)) = [];
+
     if II == 1
-        keeperMech = rate_mech;
-        keeperGeo = rate_geo;
+        keeperMech = mech_85;
+        keeperGeo = geo_85;
         keeperRate = rate_spike;
     end
+    tic
+    outCorr = bootstrp(500,@corr,[mech_85 geo_85],rate_spike','Options',opts);
+    b = toc;
+    fprintf('Correlations took %.4f seconds for %i ms\n',b,length(spikevec))
+    Corr(II).FX = outCorr(:,1);
+    Corr(II).FY = outCorr(:,2);
+    Corr(II).M = outCorr(:,3);
     
-    mechCorr = bootstrp(100,@corr,rate_mech',rate_spike','Options',opts);
-    mechCorr = mechCorr.^2;
-    mechSE = std(mechCorr);
-    geoCorr = bootstrp(100,@corr,rate_geo',rate_spike','Options',opts);
-    geoCorr = geoCorr.^2;
-    geoSE = std(geoCorr);
-    Corr(II).FX = mechCorr(:,1);
-    Corr(II).FY = mechCorr(:,2);
-    Corr(II).M = mechCorr(:,3);
-    
-    Corr(II).R = geoCorr(:,1);
-    Corr(II).TH = geoCorr(:,2);
-    
-    SE(II).FX = mechSE(:,1);
-    SE(II).FY = mechSE(:,2);
-    SE(II).M = mechSE(:,3);
-    
-    SE(II).R = geoSE(:,1);
-    SE(II).TH = geoSE(:,2);
-    
+    Corr(II).R = outCorr(:,4);
+    Corr(II).TH = outCorr(:,5);
+%     
+%     CI(II).FX = outCI(:,1);
+%     CI(II).FY = outCI(:,2);
+%     CI(II).M = outCI(:,3);
+%     
+%     CI(II).R = outCI(:,4);
+%     CI(II).TH = outCI(:,5);
+    clearvars -except Corr CI keeper* D II binsize
+    tic
+    save('Corr.mat','Corr','keeper*')
+    b = toc;
+    if b>1
+    fprintf('Saving took %f seconds\n',b)
+    end
 end
-clearvars -except Corr SE
-errorbar(mean([Corr.M]),[SE.M],'ko');ho
-errorbar(mean([Corr.FX]),[SE.FX],'ko')
-errorbar(mean([Corr.TH]),[SE.R],'ko')
-errorbar(mean([Corr.R]),[SE.TH],'ko')
-
+clearvars -except Corr CI keeper*
