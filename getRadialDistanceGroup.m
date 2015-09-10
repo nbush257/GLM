39650e9ca4fa4c3a973e3159f4f6edf9c1548319
nbush257 <nbush257@gmail.com>
2015-09-10 10:01:31 -0500

    Refactred so that the figures are cleared everytime and we dont plot over ourselves everytime

--- getRadialDistanceGroup.m
+++ getRadialDistanceGroup.m
@@ -1,3 +1,17 @@
+function [prox,med,dis] = getRadialDistanceGroup(Geo)
+%% function [prox,med,dis] = getRadialDistanceGroup(Geo)
+% Takes the Radial distance from the Geo struct and builds a GUI to
+% determine which sections are proximal, medial, and distal. Returns
+% logical vectors te length of Geo.R
+
+
+%% input handling
+if ~isstruct(Geo)
+    error('input is not a structure')
+end
+
+h = figure;
+%% Find Proximal
 plot(Geo.R)
 title('Find Most proximal, right click to exit')
 but = 0;
@@ -6,30 +20,46 @@ count = 0;
 
 prox = zeros(size(Geo.R));
 
-while but~=3
+while but~=3 % right click exits
     count = count+1;
     zoom on
     pause
+    
+    X = xlim;
+    Y = ylim;
     [x,~,but] = ginput(2);
     prox(x(1):x(2)) = 1;
+    cla
+    plot(Geo.R)
     ho
     plot(find(prox),Geo.R(find(prox)),'g')
+    
+    axx(X(1),X(2));
+    axy(Y(1),Y(2));
 end
 
-
+%% Find Distal
 dis = zeros(size(Geo.R));
-
 title('Find Most distal, right click to exit')
 but = 0;
-while but~=3
+while but~=3 % right click to exit
     count = count+1;
     zoom on
     pause
+    X = xlim;
+    Y = ylim;
     [x,~,but] = ginput(2);
     dis(x(1):x(2)) = 1;
+    cla
+    plot(Geo.R)
     ho
+    plot(find(prox),Geo.R(find(prox)),'g')
     plot(find(dis),Geo.R(find(dis)),'r')
+    axx(X(1),X(2));
+    axy(Y(1),Y(2));
+    
+    
 end
 
-
-med = C & ~prox & ~dis;
+%% get medial as exclusion of other distances
+med = ~isnan(Geo.R) & ~prox & ~dis;
